757d004e536ee3ae727bfcdecd6e9a779adc8388
Moritz Hoequist <cmhoequist@gmail.com>
2021-10-15 16:59:57 -0500

    Run code cleanup

--- GeocodeApi/Geocode/DrivenDependencies/InvalidAddressException.cs
+++ GeocodeApi/Geocode/DrivenDependencies/InvalidAddressException.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GeocodeApi.Geocode.DrivenDependencies
 {
-    public class InvalidAddressException: Exception
+    public class InvalidAddressException : Exception
     {
         public InvalidAddressException()
             : base("Address must include street, city, and two-character state code.")
--- IntegrationTests/TestHelpers/StubTestServer.cs
+++ IntegrationTests/TestHelpers/StubTestServer.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 using System.IO;
 using System.Net.Http;
 
